8161c2c855325b54d33397c604f062100857ce32
julie-j-code <jeannet.julie@gmail.com>
2019-11-07 13:23:25 +0100

    Premier commit, installation et theme bootstrap

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class HomeController extends Controller{
+
+    /**
+     * @Route("/", name="homepage")
+     */
+    public function home(){
+        return $this->render(
+            'home.html.twig'
+        );
+    }
+}
+
+
+
+
+?>
